930de2f10efc9e4a28958ed606d9acd60ae5b113
Vitaly Nikolaychuk <vitalnik@gmail.com>
2024-09-17 20:53:59 -0600

    Gradle dependencies.

--- app/src/main/kotlin/com/example/rickandmorty/app/utils/extensions/Modifier.kt
+++ app/src/main/kotlin/com/example/rickandmorty/app/utils/extensions/Modifier.kt
@@ -2,7 +2,7 @@ package com.example.rickandmorty.app.utils.extensions
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
@@ -15,7 +15,7 @@ fun Modifier.clickableWithRipple(
     clickable(
         enabled = enabled,
         interactionSource = remember { MutableInteractionSource() },
-        indication = rememberRipple(bounded = bounded),
+        indication = ripple(bounded = bounded),
         onClick = {
             onClick()
         }
