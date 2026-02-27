d2386dff85caf77f9c1d8778f95006a1844fc999
Vitaly Nikolaychuk <vitalnik@gmail.com>
2026-02-27 15:59:10 -0700

    Gradle dependencies updated.

--- app/src/main/kotlin/com/example/rickandmorty/app/ui/screens/characters/CharactersScreen.kt
+++ app/src/main/kotlin/com/example/rickandmorty/app/ui/screens/characters/CharactersScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -45,6 +46,7 @@ import com.example.rickandmorty.domain.models.CharacterModel
 import com.example.rickandmorty.domain.models.Status
 import rememberLazyListState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CharactersScreen(
     pagingItems: LazyPagingItems<CharacterModel>,
